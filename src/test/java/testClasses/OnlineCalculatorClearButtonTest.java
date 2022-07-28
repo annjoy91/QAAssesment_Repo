3edfac3fcda3e24cc7ecc7b175cd8b7bc05bedf2
ancyjoy91 <tvh8571@gmail.com>
2022-07-28 20:36:43 +0400

    df

--- src/test/java/testClasses/OnlineCalculatorClearButtonTest.java
+++ src/test/java/testClasses/OnlineCalculatorClearButtonTest.java
@@ -11,6 +11,7 @@ public class OnlineCalculatorClearButtonTest extends BaseClass {
 
 	//sampl
 	//test
+	//test
 	@BeforeMethod
 	public void setUp() {
 		initialization("https://www.online-calculator.com/full-screen-calculator/");
